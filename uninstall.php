f9a34e3e11ad9508e3946e22b1d76dff49bed9ab
Sahand Rezvan <sahand.rezvanazar@gmail.com>
2026-09-19 11:36:20 +0330

    Add uninstall handler

---
+++ uninstall.php
@@ -0,0 +1,4 @@
+<?php
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit;
+// Data is intentionally retained by default to avoid accidental loss.
+// Site owners can remove options/tables manually or through a future cleanup toggle.
